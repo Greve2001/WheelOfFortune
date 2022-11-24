cf6b35402305cdacf85a275fdf75104a54607d0f
Frederik Greve Petersen <frederik.greve@gmail.com>
2022-11-24 08:49:21 +0100

    Updated gameover screen to be more descriptive of loss and win

--- app/src/main/java/com/example/wheeloffortune/ui/screens/GameOverScreen.kt
+++ app/src/main/java/com/example/wheeloffortune/ui/screens/GameOverScreen.kt
@@ -1,21 +1,24 @@
 package com.example.wheeloffortune.ui.screens
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.wheeloffortune.WoFViewModel
+import com.example.wheeloffortune.data.GameState
 
 @Composable
 fun GameOverScreen(
     viewModel: WoFViewModel,
     onRestartGame: () -> Unit
 ){
+    val uiState = viewModel.uiState.collectAsState()
+
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -26,6 +29,19 @@ fun GameOverScreen(
             text = "Game Over!",
             fontSize = 40.sp
         )
+
+        if (uiState.value.gameState == GameState.WON) {
+            Text(text = "You have Won!")
+            Text(text = "You won with ${uiState.value.points} points")
+        }
+        if (uiState.value.gameState == GameState.LOST) {
+            Text(text = "You have Lost!")
+            Text(text = "You lost with ${uiState.value.points} points")
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(text = "The word was: ${uiState.value.hiddenWord}")
+        }
+
+        Spacer(modifier = Modifier.height(50.dp))
 
         Button(onClick = onRestartGame) {
             Text(text = "Restart Game")
